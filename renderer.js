aab8957f5b080c5aa85d95f6bf9da6f524be25e6
Chase McDonald <1chasemcd@gmail.com>
2021-01-08 13:55:37 -0800

    comment and begin work on edit feature

--- renderer.js
+++ renderer.js
@@ -5,17 +5,23 @@ db = new Datastore({filename: homedir + "/Cemetery Database/database.db", autolo
 
 
 // Variables to store database state
+let dbState = {
+  sortColIndex: 0
+};
 let sortColIndex = 0;
 let sortDirection = -1;
 let page = 0;
 let search = "";
 
-// Function to collect data in correct order based on database state variables
+// Function to collect entries in correct order based on database state variables
 function collectDocs() {
 
-  let searchedDocs
+  // Store all entries filtered by search term
+  let searchedDocs;
+
   if (search == "") {
-    searchedDocs = db.find({})
+    // If no search term is entered, then just gather all entries
+    searchedDocs = db.find({});
   } else {
     sub = RegExp(search, "i");
     searchedDocs = db.find({$or: [{ date: sub }, 
@@ -26,30 +32,32 @@ function collectDocs() {
     });
   }
 
+  // Sort previously collected entries based on the currently selected sorting criteria
   let sortedDocs;
 
   switch (sortColIndex) {
-    case 0:
+    case 0: // Sort by date
       sortedDocs = searchedDocs.sort({date: sortDirection});
       break;
   
-    case 1:
+    case 1: // Sort by deceased name
       sortedDocs = searchedDocs.sort({name: sortDirection});
       break;
   
-    case 2:
+    case 2: // Sort by contact name
       sortedDocs = searchedDocs.sort({contact: sortDirection});
       break;
 
-    case 3:
+    case 3: // Sort by contact email
       sortedDocs = searchedDocs.sort({email: sortDirection});
       break;
 
-    case 4:
+    case 4: // Sort by contact phone number (not sure why you'd want to do this, but I'll include it for consitency)
       sortedDocs = searchedDocs.sort({phone: sortDirection});
       break;
   }
 
+  // Finalize gathering of documents. Only select 50 entries at a time for performance reasons
   sortedDocs.skip(50 * page).limit(50).exec(function(err, docs) {
     loadDocsInTable(docs);
   });
@@ -57,11 +65,12 @@ function collectDocs() {
 
 // Function to add html to data table based on json from database
 function loadDocsInTable(docs) {
-  // Remove old table entry
+  // Remove old table entries
   document.querySelector("#data-table").removeChild(document.querySelector("tbody"));
-  let htmlString = ""
 
   // Append html elements with correct data to empty string
+  let htmlString = ""
+
   for (let doc of docs) {
     htmlString += "<tr>";
     
@@ -90,6 +99,7 @@ function loadDocsInTable(docs) {
   document.querySelector("#data-table").appendChild(tbody);
 }
 
+// Functions to control state of add/edit entry window
 function showAddWindow() {
   document.querySelector("#blur-content").style.filter = "blur(2px)";
   document.querySelector("#add-window").style.visibility = "visible";
@@ -100,15 +110,19 @@ function hideAddWindow() {
   document.querySelector("#add-window").style.visibility = "hidden";
 }
 
+// Function to manipulate table header html to display the correct sort icon with correct direction
 function updateSortIcon() {
+  // Remove previous downward sort icon, if it exists
   if (document.querySelector("th i.fa-sort-down")) {
     document.querySelector("th i.fa-sort-down").classList.remove("fa-sort-down");
   }
 
+  // Remove previous upward sort icon, if it exists
   if (document.querySelector("th i.fa-sort-up")) {
     document.querySelector("th i.fa-sort-up").classList.remove("fa-sort-up");
   }
 
+  // Add sort icon with correct direction to new heading
   if (sortDirection == 1) {
     headings[sortColIndex].querySelectorAll("i")[1].classList.add("fa-sort-down");
 
@@ -117,17 +131,39 @@ function updateSortIcon() {
   }
 }
 
+/**
+ * @todo clear input fields
+ */
+
+ // Function to add info in input fields into database 
 function addEntry() {
   let inputs = document.querySelectorAll("#add-window input");
 
   let doc = { date: new Date(),
-              name: inputs[0].value,
-              contact: inputs[1].value,
-              email: inputs[2].value,
-              phone: inputs[3].value
+              name: inputs[1].value + ", " + inputs[0].value,
+              contact: inputs[3].value + ", " + inputs[2].value,
+              email: inputs[4].value,
+              phone: inputs[5].value
             };
 
   db.insert(doc, function (err, newDoc) {   
+    if (err) console.log(err)
+  });
+
+  collectDocs();
+}
+
+function editEntry() {
+  let inputs = document.querySelectorAll("#add-window input");
+
+  let doc = { date: new Date(),
+              name: inputs[1].value + ", " + inputs[0].value,
+              contact: inputs[3].value + ", " + inputs[2].value,
+              email: inputs[4].value,
+              phone: inputs[5].value
+            };
+
+  db.update({}, doc, {}, function (err) {   
     if (err) console.log(err)
   });
 
@@ -141,6 +177,11 @@ document.querySelector("#close-add-button").addEventListener("click", hideAddWin
 document.querySelector("#submit-add-button").addEventListener("click", function() {
   hideAddWindow();
   addEntry();
+});
+
+document.querySelector("#submit-edit-button").addEventListener("click", function() {
+  hideAddWindow();
+  editEntry();
 });
 
 document.querySelector("#search-bar").addEventListener("input", function() {
